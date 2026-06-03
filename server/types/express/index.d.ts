e72c8235da902799cbbaf7c7222ba2a80ac157b7
senayabraha <senayabraha.w@gmail.com>
2026-06-02 19:26:10 -0700

    Extend request user role types

--- server/types/express/index.d.ts
+++ server/types/express/index.d.ts
@@ -1,11 +1,14 @@
-import { User, DeliveryPartner } from "../../generated/prisma/client.ts";
+import { DeliveryPartner, Store, UserRole } from "../../generated/prisma/client.ts";
 
 declare global {
     namespace Express {
         interface Request {
             user?: {
                 id: string;
+                email?: string;
+                role?: UserRole;
                 isAdmin?: boolean;
+                stores?: Store[];
             };
             partner?: DeliveryPartner;
         }
